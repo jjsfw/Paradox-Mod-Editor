79113c16bef5a60f5913dff0a36f54c5c97e0b02
Joshua Staynor <joshua.staynor@mail.utoronto.ca>
2018-05-07 11:44:55 -0400

    Began design of mod selection window. Currently non-functional.

---
+++ Paradox Mod Editor/Paradox Mod Editor/frmModSelect.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Paradox_Mod_Editor
+{
+    partial class frmModSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "New Mod";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Edit Mod";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(696, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "D:\\Users\\jjsfw\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\After the End " +
+    "- Sunset Invasion DLC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mod Directory:";
+            // 
+            // frmModSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmModSelect";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmModSelect";
+            this.Load += new System.EventHandler(this.frmModSelect_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Paradox Mod Editor/Paradox Mod Editor/frmModSelect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paradox_Mod_Editor
+{
+    public partial class frmModSelect : Form
+    {
+        private ParadoxTitle game;
+
+        public frmModSelect(ParadoxTitle game)
+        {
+            InitializeComponent();
+            this.game = game;
+            switch (game)
+            {
+                case ParadoxTitle.CrusaderKings:
+                    this.Text = "Crusader Kings II";
+                    break;
+                case ParadoxTitle.EuropaUniversalis:
+                    this.Text = "Europa Universalis IV";
+                    break;
+                case ParadoxTitle.HeartsOfIron:
+                    this.Text = "Hearts of Iron IV";
+                    break;
+                case ParadoxTitle.Stellaris:
+                    this.Text = "Stellaris";
+                    break;
+            }
+            this.Text += " Mod Editor";
+        }
+
+        private void frmModSelect_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
