8e917317e608bf3baac9f145f9e662c0363e5609
Joshua Staynor <joshua.staynor@mail.utoronto.ca>
2018-06-09 15:19:36 -0400

    Added abstract control factory.

---
+++ Paradox Mod Editor/Paradox Mod Editor/Models/ControlFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paradox_Mod_Editor.Models
+{
+    public abstract class ControlFactory
+    {
+        public abstract Control GetScriptObject(string scriptObject);
+
+        public static ControlFactory GetFactory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
